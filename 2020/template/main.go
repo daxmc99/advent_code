64ce72b5d0720394d387e69d59575b0808474ef9
Dax McDonald <31839142+daxmc99@users.noreply.github.com>
2020-12-03 23:18:12 -0700

    Day 1 with challenge

--- 2020/template/main.go
+++ 2020/template/main.go
@@ -11,8 +11,7 @@ const targetSum = 2020
 
 func main() {
 	data := strings.Split(input, "\n")
-	fmt.Println("Ans 1:", sum(data))
-	fmt.Println("Ans 2:", sum3(data))
+	fmt.Println(sum(data))
 }
 
 func sum(data []string) int {
@@ -31,24 +30,6 @@ func sum(data []string) int {
 			}
 			if n+m == targetSum {
 				return n * m
-			}
-		}
-	}
-	return -1
-}
-
-func sum3(data []string) int {
-	// don't have to be too smart if the lang if fast
-	for _, i := range data {
-		n1, _ := strconv.Atoi(i)
-		for _, j := range data {
-			n2, _ := strconv.Atoi(j)
-			for _, k := range data {
-				n3, _ := strconv.Atoi(k)
-
-				if n1+n2+n3 == targetSum {
-					return n1 * n2 * n3
-				}
 			}
 		}
 	}
