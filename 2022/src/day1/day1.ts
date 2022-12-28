aa4c0343b16166abf069152e6593dc92197df842
Dax McDonald` <31839142+daxmc99@users.noreply.github.com>
2022-12-28 12:09:14 -0700

    Day 1 part 1 done

---
+++ 2022/src/day1/day1.ts
@@ -0,0 +1,28 @@
+import fs from 'fs';
+
+const input = fs.readFileSync( process.cwd() + "/src/day1/"+ "puzzle1.txt", 'utf-8').toString();
+
+
+// break the input into chunks
+
+const calorieBlocks = input.split("\n\n");
+
+console.log(calorieBlocks.length);
+
+var max = 0;
+
+for (let i = 0; i < calorieBlocks.length; i++) {
+		console.log(input[i]);
+		var snacks = calorieBlocks[i].split("\n");
+
+		let sum = 0;
+		snacks.forEach((snack) => {
+			sum += parseInt(snack);
+		});
+
+		if (sum > max) {
+			max = sum;
+		}
+}
+
+console.log(max);
