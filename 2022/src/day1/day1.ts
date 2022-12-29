28b9c35ac2108eb6565877c2bacb9e0438bd4d3f
Dax McDonald` <31839142+daxmc99@users.noreply.github.com>
2022-12-28 22:43:54 -0700

    day 1 part 2 done ugly

--- 2022/src/day1/day1.ts
+++ 2022/src/day1/day1.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import {MaxHeap} from '../util/heap.js'
 
 const input = fs.readFileSync( process.cwd() + "/src/day1/"+ "puzzle1.txt", 'utf-8').toString();
 
@@ -9,20 +10,38 @@ const calorieBlocks = input.split("\n\n");
 
 console.log(calorieBlocks.length);
 
-var max = 0;
+var max = [0,0,0]
+let sums = [];
 
 for (let i = 0; i < calorieBlocks.length; i++) {
-		console.log(input[i]);
 		var snacks = calorieBlocks[i].split("\n");
 
+		console.log(snacks);
 		let sum = 0;
 		snacks.forEach((snack) => {
 			sum += parseInt(snack);
 		});
 
-		if (sum > max) {
-			max = sum;
+		console.log("the sum of the calories is " + sum);
+		sums.push(sum)
+
+    for(let j = 0; j < max.length; j++){
+			if(sum > max[j]){
+				console.log("the sum of ", sum, " is greater than ", max[j], " at index ", j)
+				max[j] = sum;
+				break;
+			}
 		}
+
 }
 
-console.log(max);
+
+console.log("the max calories are " + max);
+
+// const initialValue = 0;
+// const sumWithInitial = max.reduce(
+// 	(accumulator,currentValue) => accumulator + currentValue, initialValue );
+let sortedSums = sums.sort((a,b) => b-a);
+let top3 = sortedSums.slice(0,3);
+let sum = top3.reduce((a,b) => a+b, 0);
+console.log("the total calories carried by the top 3 elves are " + sum);
